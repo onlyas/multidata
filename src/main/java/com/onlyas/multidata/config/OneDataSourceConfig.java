68a1a9bb71a092318ee261206130455ca80056ee
Danny <wpy0213@shenzhougroup.com>
2019-04-02 10:03:51 +0800

    fix

--- src/main/java/com/onlyas/multidata/config/OneDataSourceConfig.java
+++ src/main/java/com/onlyas/multidata/config/OneDataSourceConfig.java
@@ -1,8 +1,11 @@
 package com.onlyas.multidata.config;
 
+import com.github.pagehelper.PageInterceptor;
 import com.onlyas.multidata.MultidataApplication;
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,6 +16,7 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @MapperScan(basePackages = "com.onlyas.multidata.dao.one", sqlSessionFactoryRef = "oneSqlSessionFactory", nameGenerator = MultidataApplication.SpringBeanNameGenerator.class)
@@ -35,7 +39,26 @@ public class OneDataSourceConfig {
         sessionFactory.setDataSource(dataSourceOne);
         sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:com/onlyas/multidata/dao/one/*.xml"));
         sessionFactory.setTypeAliasesPackage("com.onlyas.multidata.domain.one");
+
+        //分页插件
+        Interceptor interceptor = new PageInterceptor();
+        Properties properties = new Properties();
+        //数据库
+        properties.setProperty("helperDialect", "mysql");
+        //是否分页合理化
+        properties.setProperty("reasonable", "true");
+        properties.setProperty("supportMethodsArguments", "true");
+        properties.setProperty("params", "count=countSql");
+        interceptor.setProperties(properties);
+        sessionFactory.setPlugins(new Interceptor[]{interceptor});
+
         return sessionFactory.getObject();
     }
 
+    @Bean
+    @Primary
+    public SqlSessionTemplate oneSqlSessionTemplate() throws Exception {
+        SqlSessionTemplate template = new SqlSessionTemplate(oneSqlSessionFactory()); // 使用上面配置的Factory
+        return template;
+    }
 }
--- src/main/java/com/onlyas/multidata/config/TwoDataSourceConfig.java
+++ src/main/java/com/onlyas/multidata/config/TwoDataSourceConfig.java
@@ -1,8 +1,11 @@
 package com.onlyas.multidata.config;
 
+import com.github.pagehelper.PageInterceptor;
 import com.onlyas.multidata.MultidataApplication;
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,6 +15,7 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @MapperScan(basePackages = "com.onlyas.multidata.dao.two", sqlSessionFactoryRef = "twoSqlSessionFactory", nameGenerator = MultidataApplication.SpringBeanNameGenerator.class)
@@ -31,6 +35,25 @@ public class TwoDataSourceConfig {
         sessionFactory.setDataSource(dataSourceTwo);
         sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:com/onlyas/multidata/dao/two/*.xml"));
         sessionFactory.setTypeAliasesPackage("com.onlyas.multidata.domain.two");
+
+        //分页插件
+        Interceptor interceptor = new PageInterceptor();
+        Properties properties = new Properties();
+        //数据库
+        properties.setProperty("helperDialect", "mysql");
+        //是否分页合理化
+        properties.setProperty("reasonable", "true");
+        properties.setProperty("supportMethodsArguments", "true");
+        properties.setProperty("params", "count=countSql");
+        interceptor.setProperties(properties);
+        sessionFactory.setPlugins(new Interceptor[]{interceptor});
+
         return sessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate twoSqlSessionTemplate() throws Exception {
+        SqlSessionTemplate template = new SqlSessionTemplate(twoSqlSessionFactory()); // 使用上面配置的Factory
+        return template;
     }
 }
